698d2a1e304ddbaf8acb0a1017dc1a9b4a6264fc
kongdengyuan <kongdengyuan@gmail.com>
2019-04-12 16:48:20 +0800

    Rewrite install.sh

--- install_payflash/install.sh
+++ install_payflash/install.sh
@@ -6,10 +6,15 @@ Color_Off='\e[0m';
 # ---- High Intensity ----
 Red='\e[0;91m';  Green='\e[0;92m';
 
+SCRIPT_DIR=`dirname $0`
 CODE_DIR=/deploy/PayFlash
 SQL="psql -h localhost -U postgres"
 
-#Install x4 
+## Install dependency package
+
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+apt-get install -y nodejs npm maven  openjdk-8-jdk 
+npm install -g @angular/cli  ## for angular ui  
 
 checkRetVal () {
   if [ $? -ne 0 ]; then
@@ -21,16 +26,29 @@ checkRetVal () {
   fi
 }
 
-#cd $CODE_DIR/payment-manager 
-#./setup_x4_service.sh 
-#cp /root/HtmlPane.js /deploy/x4/ui/client/resources/sap/b/controls/panes
+#Install x4
 
-#checkRetVal 
+$SQL -c 'create schema "BYD"'
 
-#Install payment service
+cd $CODE_DIR/payment-manager 
 
+./setup_x4_service.sh 
+
+cp $SCRIPT_DIR/HtmlPane.js /deploy/x4/ui/client/resources/sap/b/controls/panes
+
+sed -i '35s#start#start >/var/log/payment/x4.log \&#' start_x4_service.sh
+
+./start_x4_service.sh 
+
+sleep 10
+
+checkRetVal 
+
+# Install payment service
+# Copy template java
+cp $SCRIPT_DIR/PaymentServiceImpl.java $CODE_DIR/payment-service/src/main/java/com/sap/sme/payment/service/impl
+cp $SCRIPT_DIR/EmailServiceImpl.java  $CODE_DIR/payment-service/src/main/java/com/sap/sme/payment/service/impl
 cd $CODE_DIR/payment-service
-#mvn package 
 $SQL  -c 'drop table "BYD"."pfPayment"'
 $SQL   --set ON_ERROR_STOP=ON -f  dbInit_PostgreSQL.sql 
 
@@ -40,6 +58,7 @@ checkRetVal
 
 cd  $CODE_DIR/payment-frontend
 npm install 
+$SQL -c "\copy \"BYD\".\"ilab/paymentmgmt/PaymentAccount\" from '$SCRIPT_DIR/pay_account_data'"
 
 checkRetVal
 
