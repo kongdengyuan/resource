11c9d26d9c98f579c61274d0b089ac3279cf0a76
kongdengyuan <kongdengyuan@gmail.com>
2019-04-17 17:19:23 +0800

    large modified

--- install_payflash/docker-postgres.sh
+++ install_payflash/docker-postgres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -ex 
 ## This script is for install postgres 11.2 on ubuntu 16.04 
 ## Need  install docker 
 
@@ -9,6 +9,16 @@
 Color_Off='\e[0m';
 # ---- High Intensity ----
 IRed='\e[0;91m';  IGreen='\e[0;92m';
+
+## ENV Setting  
+NAME=postgres
+DATA_DIR=/pgdata
+PORT=5432
+PASSWD=abcd1234
+#IMAGE_NAME=registry.kkops.cc/postgres:v1
+IMAGE_NAME=postgres:11.2
+PARA=$1
+
 
 install_docker(){
 
@@ -47,19 +57,13 @@ fi
 }
 
 install_postgres(){
-NAME=postgres
-DATA_DIR=/pgdata
-PORT=5432
-PASSWD=abcd1234
-#IMAGE_NAME=registry.kkops.cc/postgres:v1 
-IMAGE_NAME=postgres:11.2
 
 docker run -d --name $NAME \
 -p $PORT:5432 \
 -v $DATA_DIR:/var/lib/postgresql/data \
 -e POSTGRES_PASSWORD=$PASSWD $IMAGE_NAME
 
-sleep 2
+sleep 3
 
 sed -i '86s@127.0.0.1/32@0.0.0.0/0@' $DATA_DIR/pg_hba.conf
 docker restart $NAME
@@ -71,6 +75,7 @@ else
   echo -e "IRed START postgres Failed $Color_Off"      
 fi
  }
+
 install_postgres_client(){
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
@@ -85,4 +90,28 @@ else
 fi
 
 }
-check_docker
+
+uninstall_docker_postgres(){
+
+docker ps | grep $NAME &>/dev/null  && docker rm -f $NAME || echo -e "${IGreen} postgres already  uninstall $Color_Off"
+
+rm -rf $DATA_DIR && exit 0 
+
+}
+
+check_install_uninstall(){
+
+ if [ "$PARA" = "uninstall" ]; then
+    uninstall_docker_postgres 
+
+   if  [ $? -eq 0 ];then 
+      echo -e "${IGreen}Uninstall postgres  success $Color_Off" && exit 0
+    else
+      echo -e "I{Red}Uninstall postgres  Failed, Please check shell script $Color_Off" exit 1 
+  fi 
+ else 
+   check_docker
+fi
+}
+
+check_install_uninstall 
--- install_payflash/install.sh
+++ install_payflash/install.sh
@@ -2,45 +2,65 @@
 set -ex 
 ## check root user 
 [[ $EUID -ne 0 ]] && echo "Error: This script must be run as root!" && exit 1
-## Color renderin
 
+## Color renderin
 Color_Off='\e[0m';
 # ---- High Intensity ----
 Red='\e[0;91m';  Green='\e[0;92m';
 
+
 ## ENV Settings
+PARA=$1
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
-CODE_DIR=/deploy/PayFlash
+CODE_DIR=/deploy
 SQL="psql -h localhost -U postgres"
 
-## Install dependency package
+## uninstall service
+uninstall_service(){
 
+$SCRIPT_DIR/pay-frontend.sh stop
+$SCRIPT_DIR/pay-service.sh stop
+$SCRIPT_DIR/pay-manager.sh stop
+rm -r /deploy
+$SCRIPT_DIR/docker-postgres.sh uninstall
+
+}
+
+## uninstall all of service
+if [ $PARA = "uninstall" ];then
+    uninstall_service && exit 0 
+fi 
+
+## Install psotgres
+$SCRIPT_DIR/docker-postgres.sh 
+
+## Install dependency package
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 apt-get install -y nodejs  maven  openjdk-8-jdk build-essential unzip
-## install angular cli
 
-[ ! npm list -g @angular/cli | grep @angular &>/dev/null ] &&  npm install -g @angular/cli 
+## install angular cli
+! npm list -g @angular/cli | grep @angular &>/dev/null  &&  npm install -g @angular/cli 
 
 ## Pull code 
-cd $CODE_DIR/.. 
+[ ! -d $CODE_DIR  ] && mkdir $CODE_DIR
+cd $CODE_DIR 
 [ ! -d PayFlash ] &&  git clone https://github.wdf.sap.corp/ilab/PayFlash.git
 [ ! -d x4 ] && git clone https://github.wdf.sap.corp/BIG/x4.git 
 
 checkRetVal () {
-  if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
       exit $?
   else 
-  
-    echo -e "${Green}Service install success $Color_Off " &&  sleep 1 
+
+echo -e "${Green}Service install success $Color_Off " &&  sleep 1 
  
-  fi
+fi
 }
 
 #Install x4
-
 $SQL -c 'create schema "BYD"'
 
-cd $CODE_DIR/payment-manager 
+cd $CODE_DIR/PayFlash/payment-manager 
 
 cp $SCRIPT_DIR/HtmlPane.js /deploy/x4/ui/client/resources/sap/b/controls/panes
 
@@ -62,11 +82,12 @@ sleep 120
 
 # Install payment service
 # Copy template java
-cp $SCRIPT_DIR/PaymentServiceImpl.java $CODE_DIR/payment-service/src/main/java/com/sap/sme/payment/service/impl
 
-cp $SCRIPT_DIR/EmailServiceImpl.java  $CODE_DIR/payment-service/src/main/java/com/sap/sme/payment/service/impl
+cp $SCRIPT_DIR/PaymentServiceImpl.java $CODE_DIR/Payflash/payment-service/src/main/java/com/sap/sme/payment/service/impl
 
-cd $CODE_DIR/payment-service
+cp $SCRIPT_DIR/EmailServiceImpl.java  $CODE_DIR/PayFlash/payment-service/src/main/java/com/sap/sme/payment/service/impl
+
+cd $CODE_DIR/Payflash/payment-service
 
 $SQL  -c 'drop table "BYD"."pfPayment"'
 
@@ -78,7 +99,7 @@ checkRetVal
 
 #Install payment frontend
 
-cd  $CODE_DIR/payment-frontend
+cd  $CODE_DIR/PayFlash/payment-frontend
 
 npm install 
 
@@ -86,5 +107,7 @@ $SQL -c "\copy \"BYD\".\"ilab/paymentmgmt/PaymentAccount\" from '$SCRIPT_DIR/pay
 
 checkRetVal
 
-
+$SCRIPT_DIR/pay-frontend.sh start
+ 
+$SCRIPT_DIR/pay-service.sh start 
 
