4d062d9f96d2b48eea383d8bc913661046067baf
kongdengyuan <kongdengyuan@gmail.com>
2019-10-10 13:58:29 +0800

    Add docker install shell script

---
+++ docker-registry/install_docker.sh
@@ -0,0 +1,26 @@
+install_docker(){
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+# apt-cache madison docker-ce  # find docker-ce version
+sudo apt-get install docker-ce=5:19.03.3~3-0~ubuntu-xenial -y
+
+}
+
+install_docker
+
